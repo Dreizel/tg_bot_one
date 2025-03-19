5e431cd3ccbc67c60b27f4c2c972d59278bdc949
Ali <ganievru67@gmail.com>
2025-03-19 06:04:48 +0300

    Доработка

--- bot.py
+++ bot.py
@@ -1,10 +1,17 @@
+import os
+
 from handlers import router
-from config import BOT_TOKEN
 
 from aiogram.enums import ParseMode
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.client.default import DefaultBotProperties
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+
+if not BOT_TOKEN:
+    raise ValueError("Переменная окружения BOT_TOKEN не найдена! Установите её в Railway.")
+
 
 bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 storage = MemoryStorage()
